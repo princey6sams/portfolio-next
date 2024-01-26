ff4949d5c96d965d7bc9c4ed3e64d61407942b1a
princey6sams <110205359+princey6sams@users.noreply.github.com>
2024-01-25 22:52:10 -0330

    Put config back to local optima

--- next.config.js
+++ next.config.js
@@ -3,9 +3,21 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
+    images: {
+        unoptimized: true,
+        // formats: ['image/avif', 'image/webp'],
+        // remotePatterns: [
+        //     {
+        //         protocol: 'https',
+        //         hostname: 'images.prismic.io',
+        //         port: '',
+        //         pathname: 'image/upload/**',
+        //     },
+        // ],
+    },
     basePath: isProd ? "/portfolio-next" : "",
     distDir: "dist",
-    //output: "export", // Uncomment before committing to avoid next build error // comment before deploying to vercel or 
+    output: "export", // Uncomment before committing to avoid next build error // comment before deploying to vercel or 
     experimental: {
         serverComponentsExternalPackages: ["mongoose"]
     },
