6d5da98df83bd00e352d3660bf863b95059ffac9
princey6sams <110205359+princey6sams@users.noreply.github.com>
2024-01-25 22:41:28 -0330

    Route modification to blame

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
     basePath: isProd ? "/portfolio-next" : "",
     distDir: "dist",
-    output: "export", // Uncomment before committing to avoid next build error // comment before deploying to vercel or 
+    //output: "export", // Uncomment before committing to avoid next build error // comment before deploying to vercel or 
     experimental: {
         serverComponentsExternalPackages: ["mongoose"]
     },
