8ca075d7d8d559e165b0784a71791da203477113
princey6sams <princeasingha@gmail.com>
2024-01-26 14:56:31 -0330

    Refactor to modularize projects done.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -15,75 +15,68 @@ import { Stack, Typography } from '@mui/material';
 const imgPath = replaceBase("${Base}/ig.jpeg");
 const basis = "md:basis-1 lg:basis-1/2"
 
+interface Project {
+    title: string;
+    description: string;
+    date: string;
+    imgPath: string;
+}
+
+// To come from db eventually
+const projects = [
+    {
+        title: "Project 1",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum sem magna, ut porttitor nisi interdum facilisis. Nullam feugiat faucibus vestibulum. Nulla accumsan velit id urna faucibus imperdiet. Etiam ac tortor eget lectus rhoncus tempus quis vel eros. Integer commodo, erat vitae ullamcorper interdum, sapien lorem condimentum ante, quis lobortis elit lacus sit amet diam.",
+        date: "Aug 2023",
+        imgPath: "${Base}/DSC_3418-01.jpeg" // Maybe transform paths before storing to db using middleware???
+    },
+    {
+        title: "Project 2",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum sem magna, ut porttitor nisi interdum facilisis. Nullam feugiat faucibus vestibulum. Nulla accumsan velit id urna faucibus imperdiet. Etiam ac tortor eget lectus rhoncus tempus quis vel eros. Integer commodo, erat vitae ullamcorper interdum, sapien lorem condimentum ante, quis lobortis elit lacus sit amet diam.",
+        date: "Aug 2023",
+        imgPath: "${Base}/ig.jpeg" // Maybe change to a video link after youtube player integration.
+    },
+    {
+        title: "Project 3",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum sem magna, ut porttitor nisi interdum facilisis. Nullam feugiat faucibus vestibulum. Nulla accumsan velit id urna faucibus imperdiet. Etiam ac tortor eget lectus rhoncus tempus quis vel eros. Integer commodo, erat vitae ullamcorper interdum, sapien lorem condimentum ante, quis lobortis elit lacus sit amet diam.",
+        date: "Aug 2023",
+        imgPath: "${Base}/DSC_4145-01.jpeg"
+    },
+    {
+        title: "Project 4",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum sem magna, ut porttitor nisi interdum facilisis. Nullam feugiat faucibus vestibulum. Nulla accumsan velit id urna faucibus imperdiet. Etiam ac tortor eget lectus rhoncus tempus quis vel eros. Integer commodo, erat vitae ullamcorper interdum, sapien lorem condimentum ante, quis lobortis elit lacus sit amet diam.",
+        date: "Aug 2023",
+        imgPath: "${Base}/1673305619062-01.jpeg"
+    },
+]
+
+const ProjectBuilder = (project: Project) => (
+    <CarouselItem className={basis}>
+        <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
+            <Card className="w-[38rem]">
+                <CardContent className='p-4'>
+                    <Image src={replaceBase(project.imgPath)} alt="hero" height="510" width="764"/>
+                </CardContent>
+            </Card>
+            <Stack direction="row" spacing={51} alignItems="center" justifyContent="center">
+                <Typography fontSize={30}>{project.title}</Typography>
+                <CardDescription>{project.date}</CardDescription>
+            </Stack>
+            <CardContent>
+                <Typography>{project.description}</Typography>
+            </CardContent>
+        </Card>
+    </CarouselItem>
+);
+
 const ProjectCard = () => {
   return (
     <Carousel style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: "90rem", height: "40rem"}} >
         <CarouselPrevious />
         <CarouselContent>
-        <CarouselItem className={basis}>
-            <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
-                <Card className="w-[38rem]">
-                    <CardContent className='p-4'>
-                        <Image src="/DSC_3418-01.jpeg" alt="hero" height="510" width="764"/>
-                    </CardContent>
-                </Card>
-                <Stack direction="row" spacing={51} alignItems="center" justifyContent="center">
-                    <Typography fontSize={30}>Project 1</Typography>
-                    <CardDescription>Aug 2023</CardDescription>
-                </Stack>
-                <CardContent>
-                    <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum sem magna, ut porttitor nisi interdum facilisis. Nullam feugiat faucibus vestibulum. Nulla accumsan velit id urna faucibus imperdiet. Etiam ac tortor eget lectus rhoncus tempus quis vel eros. Integer commodo, erat vitae ullamcorper interdum, sapien lorem condimentum ante, quis lobortis elit lacus sit amet diam.</Typography>
-                </CardContent>
-            </Card>
-        </CarouselItem>
-        <CarouselItem className={basis}>
-            <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
-                <Card className="w-[38rem]">
-                <CardContent className='p-4'>
-                    <Image src={imgPath} alt="hero" height="510" width="764"/>
-                </CardContent>
-                </Card>
-                <Stack direction="row" spacing={51} alignItems="center" justifyContent="center">
-                    <Typography fontSize={30}>Project 2</Typography>
-                    <CardDescription>Aug 2023</CardDescription>
-                </Stack>
-                <CardContent>
-                    <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum sem magna, ut porttitor nisi interdum facilisis. Nullam feugiat faucibus vestibulum. Nulla accumsan velit id urna faucibus imperdiet. Etiam ac tortor eget lectus rhoncus tempus quis vel eros. Integer commodo, erat vitae ullamcorper interdum, sapien lorem condimentum ante, quis lobortis elit lacus sit amet diam.</Typography>
-                </CardContent>
-            </Card>
-        </CarouselItem>
-        <CarouselItem className={basis}>
-            <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
-                <Card className="w-[38rem]">
-                    <CardContent className='p-4'>
-                        <Image src="/DSC_4145-01.jpeg" alt="hero" height="510" width="764"/>
-                    </CardContent>
-                </Card>
-                <Stack direction="row" spacing={51} alignItems="center" justifyContent="center">
-                    <Typography fontSize={30}>Project 3</Typography>
-                    <CardDescription>Aug 2023</CardDescription>
-                </Stack>
-                <CardContent>
-                    <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum sem magna, ut porttitor nisi interdum facilisis. Nullam feugiat faucibus vestibulum. Nulla accumsan velit id urna faucibus imperdiet. Etiam ac tortor eget lectus rhoncus tempus quis vel eros. Integer commodo, erat vitae ullamcorper interdum, sapien lorem condimentum ante, quis lobortis elit lacus sit amet diam.</Typography>
-                </CardContent>
-            </Card>
-        </CarouselItem>
-        <CarouselItem className={basis}>
-            <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
-                <Card className="w-[38rem]">
-                <CardContent className='p-4'>
-                    <Image src="/1673305619062-01.jpeg" alt="hero" height="510" width="764"/>
-                </CardContent>
-                </Card>
-                <Stack direction="row" spacing={51} alignItems="center" justifyContent="center">
-                    <Typography fontSize={30}>Project 4</Typography>
-                    <CardDescription>Aug 2023</CardDescription>
-                </Stack>
-                <CardContent>
-                    <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vestibulum sem magna, ut porttitor nisi interdum facilisis. Nullam feugiat faucibus vestibulum. Nulla accumsan velit id urna faucibus imperdiet. Etiam ac tortor eget lectus rhoncus tempus quis vel eros. Integer commodo, erat vitae ullamcorper interdum, sapien lorem condimentum ante, quis lobortis elit lacus sit amet diam.</Typography>
-                </CardContent>
-            </Card>
-        </CarouselItem>
+           {projects.map((project, index) => (
+                <ProjectBuilder key={index} {...project} />
+           ))}
         </CarouselContent>
         <CarouselNext />
     </Carousel>
