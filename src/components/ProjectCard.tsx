e91241286e5e39ef20b4521b17ed4d31b7b18163
princey6sams <princeasingha@gmail.com>
2024-01-26 14:36:00 -0330

    Fixed some spacing issues.

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -21,11 +21,11 @@ const ProjectCard = () => {
         <CarouselPrevious />
         <CarouselContent>
         <CarouselItem className={basis}>
-            <Card className="w-[40rem] h-[650px]" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
+            <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
                 <Card className="w-[38rem]">
-                <CardContent className='p-4'>
-                    <Image src="/DSC_3418-01.jpeg" alt="hero" height="510" width="764"/>
-                </CardContent>
+                    <CardContent className='p-4'>
+                        <Image src="/DSC_3418-01.jpeg" alt="hero" height="510" width="764"/>
+                    </CardContent>
                 </Card>
                 <Stack direction="row" spacing={51} alignItems="center" justifyContent="center">
                     <Typography fontSize={30}>Project 1</Typography>
@@ -37,7 +37,7 @@ const ProjectCard = () => {
             </Card>
         </CarouselItem>
         <CarouselItem className={basis}>
-            <Card className="w-[40rem] h-[650px]" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
+            <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
                 <Card className="w-[38rem]">
                 <CardContent className='p-4'>
                     <Image src={imgPath} alt="hero" height="510" width="764"/>
@@ -53,7 +53,7 @@ const ProjectCard = () => {
             </Card>
         </CarouselItem>
         <CarouselItem className={basis}>
-            <Card className="w-[40rem] h-[650px]" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
+            <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
                 <Card className="w-[38rem]">
                     <CardContent className='p-4'>
                         <Image src="/DSC_4145-01.jpeg" alt="hero" height="510" width="764"/>
@@ -69,7 +69,7 @@ const ProjectCard = () => {
             </Card>
         </CarouselItem>
         <CarouselItem className={basis}>
-            <Card className="w-[40rem] h-[650px]" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
+            <Card className="w-[40rem] h-[650px] pt-3" style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>  
                 <Card className="w-[38rem]">
                 <CardContent className='p-4'>
                     <Image src="/1673305619062-01.jpeg" alt="hero" height="510" width="764"/>
