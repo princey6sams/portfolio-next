1c36d868dbafa7aefc6687146105d947128e4e2f
princey6sams <110205359+princey6sams@users.noreply.github.com>
2024-01-25 22:30:32 -0330

    Hopefully fixes build.

--- src/components/IntroBlock.tsx
+++ src/components/IntroBlock.tsx
@@ -1,19 +1,21 @@
 import { Container, Stack, Typography } from "@mui/material";
 import CustomButton from "./CustomButton";
 
-export default function IntroBlock() {
+const IntroBlock = (  ) => {
     return (
-      <Stack direction="row" justifyContent="space-around" pt={10} px={20}>
-        <Container maxWidth="md">
-          <Typography variant="h4" fontWeight="bold" fontSize={50}>Combining computer engineering and photography for impact.</Typography>
-        </Container>
-        <Container maxWidth="md">
-          <Typography variant="h6">Welcome to my portfolio website, where I showcase my unique blend of computer engineering and photography. Explore my work and projects to see how I bring creativity and technical expertise together.</Typography>
-          <Stack direction="row" spacing={4} pt={4}>
-            <CustomButton link="/about" name="Explore" variant="default" width="150px"/>
-            <CustomButton link="/contact" name="Contact" variant="outline" width="150px"/>
-          </Stack>
-        </Container>
-      </Stack>
+      <>
+        <Stack direction="row" justifyContent="space-around" pt={10} px={20}>
+          <Container maxWidth="md">
+            <Typography variant="h4" fontWeight="bold" fontSize={50}>Combining computer engineering and photography for impact.</Typography>
+          </Container>
+          <Container maxWidth="md">
+            <Typography variant="h6">Welcome to my portfolio website, where I showcase my unique blend of computer engineering and photography. Explore my work and projects to see how I bring creativity and technical expertise together.</Typography>
+            <Stack direction="row" spacing={4} pt={4}>
+              <CustomButton link="/about" name="Explore" variant="default" width="150px"/>
+              <CustomButton link="/contact" name="Contact" variant="outline" width="150px"/>
+            </Stack>
+          </Container>
+        </Stack>
+      </>
     )
 };
