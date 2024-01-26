9358b5ad9eba5d0bbf96eeef19dd16e7b942ce64
princey6sams <110205359+princey6sams@users.noreply.github.com>
2024-01-25 22:37:26 -0330

    That didn't do it, so this...

--- next.config.js
+++ next.config.js
@@ -3,21 +3,9 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-    images: {
-        unoptimized: true,
-        // formats: ['image/avif', 'image/webp'],
-        // remotePatterns: [
-        //     {
-        //         protocol: 'https',
-        //         hostname: 'images.prismic.io',
-        //         port: '',
-        //         pathname: 'image/upload/**',
-        //     },
-        // ],
-    },
     basePath: isProd ? "/portfolio-next" : "",
     distDir: "dist",
-    output: "export", // Uncomment before committing to avoid next build error
+    output: "export", // Uncomment before committing to avoid next build error // comment before deploying to vercel or 
     experimental: {
         serverComponentsExternalPackages: ["mongoose"]
     },
